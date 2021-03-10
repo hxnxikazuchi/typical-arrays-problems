a9459fc2d8ee25cc83e8c0d2d457bbf68873a278
Ruslan Kupzhassarov <kupzhasarov14@gmail.com>
2021-03-10 21:34:48 +0600

    feat: create a code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let min = array[0];
+        for (let i = 1; i < array.length; ++i) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
 
-exports.min = function min (array) {
-  return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let max = array[0];
+        for (let i = 1; i < array.length; ++i) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let avg = 0;
+        for (let i = 0; i < array.length; ++i) {
+            avg = avg + array[i];
+        }
+        let length = array.length;
+        let sum = avg / length;
+        return sum;
+    }
 }
